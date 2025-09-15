24cc3a99ee1020f89cde746fea5e7de57540e822
emorozov28 <emorozov328@gmail.com>
2025-09-15 17:28:18 +0200

    init

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+
+import '@/app/styles/index.scss'
+import App from "@/app/App";
+import {Provider} from "react-redux";
+import store from "@/app/store";
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
+)
